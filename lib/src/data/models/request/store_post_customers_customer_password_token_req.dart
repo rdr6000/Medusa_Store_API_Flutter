26b3c0621dbde169726307943874abc312290fea
rdr6000 <rdr6000@gmail.com>
2024-08-05 06:27:47 +0530

    update password req

--- lib/src/data/models/request/store_post_customers_customer_password_token_req.dart
+++ lib/src/data/models/request/store_post_customers_customer_password_token_req.dart
@@ -1,16 +1,23 @@
 class StorePostCustomersCustomerPasswordTokenReq {
   String? email;
+  String? password;
+  String? token;
 
-  StorePostCustomersCustomerPasswordTokenReq({this.email});
+  StorePostCustomersCustomerPasswordTokenReq(
+      {this.email, this.password, this.token});
 
   StorePostCustomersCustomerPasswordTokenReq.fromJson(
       Map<String, dynamic> json) {
     email = json['email'];
+    password = json['password'];
+    token = json['token'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['email'] = email;
+    data['password'] = password;
+    data['token'] = token;
     return data;
   }
 }
