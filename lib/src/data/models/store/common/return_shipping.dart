c9d5c659e757779a0a3982610c2f0fe4c0c0ec7b
rdr6000 <rdr6000@gmail.com>
2024-09-11 22:04:40 +0530

    return shipping remove price

--- lib/src/data/models/store/common/return_shipping.dart
+++ lib/src/data/models/store/common/return_shipping.dart
@@ -1,17 +1,15 @@
 class ReturnShipping {
   final String? optionId;
-  final int? price;
 
-  ReturnShipping({this.optionId, this.price});
+  ReturnShipping({this.optionId});
 
   factory ReturnShipping.fromJson(Map<String, dynamic> json) {
-    return ReturnShipping(optionId: json['option_id'], price:json['price'] );
+    return ReturnShipping(optionId: json['option_id']);
   }
 
   Map<String, dynamic> toJson() {
     var json = <String, dynamic>{};
     json['option_id'] = optionId;
-    json['price'] = price;
     return json;
   }
 }
