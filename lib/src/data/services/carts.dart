2a0213723b59a9af85f4d4ebeedc30b00fc63caf
rdr6000 <rdr6000@gmail.com>
2025-04-25 15:19:52 +0530

    feat: add new fields to ProductCollection model for enhanced product details

--- lib/src/data/services/carts.dart
+++ lib/src/data/services/carts.dart
@@ -12,13 +12,15 @@ class CartsResource extends BaseResource {
   /// @param {StorePostCartsCartShippingMethodReq} payload Containing id of shipping option and optional data
   /// @param customHeaders
   /// @return {ResponsePromise<StoreCartsRes>}
-  Future<StoreCartsRes?> addShippingMethod(
-      {required String cartId, StorePostCartsCartShippingMethodReq? req, Map<String, dynamic>? customHeaders}) async {
+  Future<StoreCartsRes?> addShippingMethod({required String cartId,
+    StorePostCartsCartShippingMethodReq? req,
+    Map<String, dynamic>? customHeaders}) async {
     try {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      final response = await client.post('/store/carts/$cartId/shipping-methods', data: req);
+      final response =
+          await client.post('/store/carts/$cartId/shipping-methods', data: req);
       if (response.statusCode == 200) {
         return StoreCartsRes.fromJson(response.data);
       } else {
@@ -40,7 +42,8 @@ class CartsResource extends BaseResource {
   /// An idempotency key will be generated if none is provided in the header Idempotency-Key and added to the response.
   /// If an error occurs during cart completion or the request is interrupted for any reason,
   /// the cart completion can be retried by passing the idempotency key in the Idempotency-Key header.
-  Future<StoreCompleteCartRes?> complete({required String cartId, Map<String, dynamic>? customHeaders}) async {
+  Future<StoreCompleteCartRes?> complete(
+      {required String cartId, Map<String, dynamic>? customHeaders}) async {
     try {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
@@ -60,7 +63,8 @@ class CartsResource extends BaseResource {
   /// Create a Cart. Although optional, specifying the cart's region and sales channel can affect the cart's pricing and the products that can be added to the cart respectively. So, make sure to set those early on and change them if necessary, such as when the customer changes their region.
   ///
   /// If a customer is logged in, the cart's customer ID and email will automatically be set.
-  Future<StoreCartsRes?> createCart({StorePostCartReq? req, Map<String, dynamic>? customHeaders}) async {
+  Future<StoreCartsRes?> createCart(
+      {StorePostCartReq? req, Map<String, dynamic>? customHeaders}) async {
     try {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
@@ -83,12 +87,14 @@ class CartsResource extends BaseResource {
   /// @param {string} cart_id is required
   /// @param customHeaders
   /// @return {ResponsePromise<StoreCartsRes>}
-  Future<StoreCartsRes?> createPaymentSessions({required String cartId, Map<String, dynamic>? customHeaders}) async {
+  Future<StoreCartsRes?> createPaymentSessions(
+      {required String cartId, Map<String, dynamic>? customHeaders}) async {
     try {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      final response = await client.post('/store/carts/$cartId/payment-sessions');
+      final response =
+          await client.post('/store/carts/$cartId/payment-sessions');
       if (response.statusCode == 200) {
         return StoreCartsRes.fromJson(response.data);
       } else {
@@ -105,13 +111,15 @@ class CartsResource extends BaseResource {
   /// @param {string} code discount code to remove
   /// @param customHeaders
   /// @return {ResponsePromise<StoreCartsRes>}
-  Future<StoreCartsRes?> deleteDiscount(
-      {required String cartId, String? code, Map<String, dynamic>? customHeaders}) async {
+  Future<StoreCartsRes?> deleteDiscount({required String cartId,
+    String? code,
+    Map<String, dynamic>? customHeaders}) async {
     try {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      final response = await client.delete('/store/carts/$cartId/discounts/$code');
+      final response =
+          await client.delete('/store/carts/$cartId/discounts/$code');
       if (response.statusCode == 200) {
         return StoreCartsRes.fromJson(response.data);
       } else {
@@ -129,13 +137,15 @@ class CartsResource extends BaseResource {
   /// @param {string} provider_id the provider id of the session e.g. "stripe"
   /// @param customHeaders
   /// @return {ResponsePromise<StoreCartsRes>}
-  Future<StoreCartsRes?> deletePaymentSession(
-      {required String cartId, required String providerId, Map<String, dynamic>? customHeaders}) async {
+  Future<StoreCartsRes?> deletePaymentSession({required String cartId,
+    required String providerId,
+    Map<String, dynamic>? customHeaders}) async {
     try {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      final response = await client.delete('/store/carts/$cartId/payment-sessions/$providerId');
+      final response = await client
+          .delete('/store/carts/$cartId/payment-sessions/$providerId');
       if (response.statusCode == 200) {
         return StoreCartsRes.fromJson(response.data);
       } else {
@@ -152,13 +162,15 @@ class CartsResource extends BaseResource {
   /// @param {string} provider_id the provider id of the session e.g. "stripe"
   /// @param customHeaders
   /// @return {ResponsePromise<StoreCartsRes>}
-  Future<StoreCartsRes?> refreshPaymentSession(
-      {required String cartId, required String providerId, Map<String, dynamic>? customHeaders}) async {
+  Future<StoreCartsRes?> refreshPaymentSession({required String cartId,
+    required String providerId,
+    Map<String, dynamic>? customHeaders}) async {
     try {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      final response = await client.post('/store/carts/$cartId/payment-sessions/$providerId/refresh');
+      final response = await client
+          .post('/store/carts/$cartId/payment-sessions/$providerId/refresh');
       if (response.statusCode == 200) {
         return StoreCartsRes.fromJson(response.data);
       } else {
@@ -174,7 +186,8 @@ class CartsResource extends BaseResource {
   /// @param {string} cart_id is required
   /// @param customHeaders
   /// @return {ResponsePromise<StoreCartsRes>}
-  Future<StoreCartsRes?> retrieve({required String cartId, Map<String, dynamic>? customHeaders}) async {
+  Future<StoreCartsRes?> retrieve(
+      {required String cartId, Map<String, dynamic>? customHeaders}) async {
     try {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
@@ -204,12 +217,12 @@ class CartsResource extends BaseResource {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      print("req: ${req!.toJson()}");
-      print("cartId: $cartId");
+      log("cartId: $cartId");
+      log("req: ${req!.toJson().toString()}");
       final response =
           await client.post('/store/carts/$cartId/payment-session', data: req);
       if (response.statusCode == 200) {
-        print("response: ${response.data}");
+        log("response: ${response.data}");
         return StoreCartsRes.fromJson(response.data);
       } else {
         throw response;
@@ -225,8 +238,9 @@ class CartsResource extends BaseResource {
   /// @param {StorePostCartsCartReq} payload is required and can contain region_id, email, billing and shipping address
   /// @param customHeaders
   /// @return {ResponsePromise<StoreCartsRes>}
-  Future<StoreCartsRes?> update(
-      {required String cartId, required StorePostCartsCartReq? req, Map<String, dynamic>? customHeaders}) async {
+  Future<StoreCartsRes?> update({required String cartId,
+    required StorePostCartsCartReq? req,
+    Map<String, dynamic>? customHeaders}) async {
     try {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
@@ -259,7 +273,8 @@ class CartsResource extends BaseResource {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      final response = await client.post('/store/carts/$cartId/payment-sessions/$providerId', data: req);
+      final response = await client
+          .post('/store/carts/$cartId/payment-sessions/$providerId', data: req);
       if (response.statusCode == 200) {
         return StoreCartsRes.fromJson(response.data);
       } else {
@@ -289,7 +304,8 @@ class CartsResource extends BaseResource {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      final response = await client.post('/store/carts/$cartId/line-items', data: {
+      final response =
+          await client.post('/store/carts/$cartId/line-items', data: {
         'variant_id': variantId,
         'quantity': quantity,
       });
@@ -322,7 +338,9 @@ class CartsResource extends BaseResource {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      final response = await client.post('/store/carts/$cartId/line-items/$lineId', data: {'quantity': quantity});
+      final response = await client.post(
+          '/store/carts/$cartId/line-items/$lineId',
+          data: {'quantity': quantity});
       if (response.statusCode == 200) {
         return StoreCartsRes.fromJson(response.data);
       } else {
@@ -349,7 +367,8 @@ class CartsResource extends BaseResource {
       if (customHeaders != null) {
         client.options.headers.addAll(customHeaders);
       }
-      final response = await client.delete('/store/carts/$cartId/line-items/$lineId');
+      final response =
+          await client.delete('/store/carts/$cartId/line-items/$lineId');
       if (response.statusCode == 200) {
         return StoreCartsRes.fromJson(response.data);
       } else {
